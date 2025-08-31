75668a4f32dd8cd3852f09c6f05148415a8bdfe5
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-08-31 16:54:32 +0800

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// MongoDB Verbindung
+mongoose.connect('mongodb://localhost:27017/offene-bibliothek', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB verbunden'))
+    .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('Willkommen zur Offenen Bibliothek Katalog API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}`);
+});
